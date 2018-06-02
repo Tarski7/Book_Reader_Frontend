7d430e4b839b75c43ecea2026841f42827b514c2
Michał Tarkowski <tarski9@gmail.com>
2018-06-02 17:41:29 +0200

    edit method

--- front2/js/app.js
+++ front2/js/app.js
@@ -21,15 +21,24 @@ function insertBooks(listElem, books){
         editButton.innerText = 'edit';
         editButton.classList.add('edit');
         editButton.classList.add('btn');
-        editButton.classList.add('btn-warning');  
+        editButton.classList.add('btn-warning'); 
+        
+        var hideButton = document.createElement('button');
+        hideButton.innerText = 'hide';
+        hideButton.classList.add('hideB');
+        hideButton.classList.add('btn');
+        hideButton.classList.add('btn-info');
 
         let li = document.createElement('li');
         li.dataset.id=book.id;
 
         li.appendChild(title);
-        li.appendChild(clickButton); 
+        li.appendChild(clickButton);
+        li.appendChild(hideButton);
         li.appendChild(deleteButton);
         li.appendChild(editButton);
+        
+        hideButton.style.display = 'none';
         
         listElem.appendChild(li);
     });
@@ -42,32 +51,39 @@ function showDiv( book, id){
 	for (var i = 0; i < li.length; i++) {
 		if (li[i].getAttribute("data-id") == id) {
 			thisLi = li[i];
-			thisLi.addEventListener('click', function(e) {
+			/*thisLi.addEventListener('click', function(e) {
 		    	e.stopPropagation();
-		    })
+		    })*/
 		}
 	}
 	
 	var author = document.createElement('h5');
     author.innerText = book.author;
+    author.classList.add('author');
     var authorLabel = document.createElement('h5');
     authorLabel.innerText = "Author:";
     
     var isbn = document.createElement('h5');
     isbn.innerText = book.isbn;
+    isbn.classList.add('isbn');
     var isbnLabel = document.createElement('h5');
     isbnLabel.innerText = "ISBN:";
     
     var publisher = document.createElement('h5');
     publisher.innerText = book.publisher;
+    publisher.classList.add('publisher');
     var publisherLabel = document.createElement('h5');
     publisherLabel.innerText = "Publisher:";
 
     var type = document.createElement('h5');
     type.innerText = book.type;
+    type.classList.add('type');
     var typeLabel = document.createElement('h5');
     typeLabel.innerText = "Type:";
 
+    hideButton = thisLi.querySelector('.hideB');
+    hideButton.style.display = 'initial';
+    
     thisLi.appendChild(authorLabel);
     thisLi.appendChild(author);
     thisLi.appendChild(isbnLabel);
@@ -76,66 +92,32 @@ function showDiv( book, id){
     thisLi.appendChild(publisher);
     thisLi.appendChild(typeLabel);
     thisLi.appendChild(type);
-	
-
-    /*var authorLabel = document.createElement('p');
-    authorLabel.innerText = "Author:";
     
-    var isbnLabel = document.createElement('p');
-    isbnLabel.innerText = "ISBN:";
+    var clickButton = thisLi.querySelector('.btn-success');
+    clickButton.style.display = 'none';
     
-    var publisherLabel = document.createElement('p');
-    publisherLabel.innerText = "Publisher:";
-
-    var typeLabel = document.createElement('p');
-    typeLabel.innerText = "Type:";
-
-    thisLi.appendChild(authorLabel);
-    thisLi.appendChild(isbnLabel);
-    thisLi.appendChild(publisherLabel);
-    thisLi.appendChild(typeLabel);
-    
-    var author = document.createElement('p');
-    author.innerText = book.author;
-    
-    var isbn = document.createElement('p');
-    isbn.innerText = book.isbn;
-    
-    var publisher = document.createElement('p');
-    publisher.innerText = book.publisher;
-    
-    var type = document.createElement('p');
-    type.innerText = book.type;
-    
-    authorLabel.appendChild(author);
-    isbnLabel.appendChild(author);
-    publisherLabel.appendChild(author);
-    typeLabel.appendChild(author);*/
-}
-
-function editDiv( book, id){
-
-	var thisLi;
-	var li = document.querySelectorAll('li');
-	for (var i = 0; i < li.length; i++) {
-		if (li[i].getAttribute("data-id") == id) {
-			thisLi = li[i];
-			thisLi.addEventListener('click', function(e) {
-		    	e.stopPropagation();
-		    })
-		}
-	}
-	
-    var title2 = document.querySelector('h5');
-    title2.innerText = book.title;
-	
+    $(hideButton).on('click', function(eee){
+    	
+        if (eee.target.classList == 'hideB btn btn-info') {
+        	authorLabel.style.display = 'none';
+        	author.style.display = 'none';
+        	isbnLabel.style.display = 'none'; 
+        	isbn.style.display = 'none'; 
+        	publisherLabel.style.display = 'none'; 
+        	publisher.style.display = 'none'; 
+        	typeLabel.style.display = 'none'; 
+        	type.style.display = 'none'; 
+        	hideButton.style.display = 'none';
+        	clickButton.style.display = 'initial';
+        }
+        
+    });
 }
 
 
 document.addEventListener("DOMContentLoaded",function(){
 	
 	var ul = document.querySelector('ul.books');
-	//var tt = document.querySelector('div.tt');
 	
 	$.ajax({
 		url: "http://localhost:8080/books2/",
@@ -143,10 +125,8 @@ document.addEventListener("DOMContentLoaded",function(){
 		dataType: "json"
 	})
 	.done( books => insertBooks(ul, books) );
-	//.done( books => insertBooks(tt, books) );
 	
 	$(ul).on('click', function(e){
-	//$(tt).on('click', function(e){
         if(e.target.classList == 'more btn btn-success') {	
             var parent = e.target.parentElement;
             var id = parent.getAttribute("data-id");
@@ -171,6 +151,23 @@ document.addEventListener("DOMContentLoaded",function(){
               
             var h3 = parent.querySelector('h3');
             h3.setAttribute('contenteditable', true);
+            h3.style = "background-color:grey";
+            
+            var h5author = parent.querySelector('.author');
+            h5author.setAttribute('contenteditable', true);
+            h5author.style = "background-color:grey";
+            
+            var h5isbn = parent.querySelector('.isbn');
+            h5isbn.setAttribute('contenteditable', true);
+            h5isbn.style = "background-color:grey";
+            
+            var h5publisher = parent.querySelector('.publisher');
+            h5publisher.setAttribute('contenteditable', true);
+            h5publisher.style = "background-color:grey";
+            
+            var h5type = parent.querySelector('.type');
+            h5type.setAttribute('contenteditable', true);
+            h5type.style = "background-color:grey";
             
             var saveButton = document.createElement('button');
             saveButton.innerText = 'save';
@@ -184,12 +181,19 @@ document.addEventListener("DOMContentLoaded",function(){
             if (eee.target.classList == 'save btn btn-info') {
             	
             	var title = parent.querySelector('h3').innerText;
-            	console.log(title);
+            	var author = parent.querySelector('.author').innerText;
+            	var isbn = parent.querySelector('.isbn').innerText;
+            	var publisher = parent.querySelector('.publisher').innerText;
+            	var type = parent.querySelector('.type').innerText;
             	
 	            $.ajax({
 	        		url: "http://localhost:8080/books2/" + id,
 	        		data: JSON.stringify({
-	                    "title" : title}),
+	                    "title" : title,
+	                    "author" : author,
+	                    "publisher" : publisher,
+	                    "type" : type,
+	                    "isbn" : isbn}),
 	        		contentType: "application/json",
 	        		type: "PUT",
 	        		dataType: "json"
